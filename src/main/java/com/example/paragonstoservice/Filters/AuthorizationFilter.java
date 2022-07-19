902d68cd375f98f72a28c4f7a3835d03947b5a30
Jotaro <zibble@mail.ru>
2022-07-19 21:17:17 +0400

    ФИКС Авторизации

--- src/main/java/com/example/paragonstoservice/Filters/AuthorizationFilter.java
+++ src/main/java/com/example/paragonstoservice/Filters/AuthorizationFilter.java
@@ -82,23 +82,31 @@ public class AuthorizationFilter extends OncePerRequestFilter {
             String token = authHeader.substring(7);
             if(adminFilter(request)) {
                 System.out.println("ADMIN REQUEST");
-                if (tokenService.checkRole(token).equals("ADMIN"))
+                if (tokenService.checkRole(token).equals("ADMIN")) {
                     filterChain.doFilter(request, response);
+                    return;
+                }
             }
             if(moderatorFilter(request)) {
                 System.out.println("MODERATOR REQUEST");
-                if (tokenService.checkRole(token).equals("MODERATOR"))
+                if (tokenService.checkRole(token).equals("MODERATOR")) {
                     filterChain.doFilter(request, response);
+                    return;
+                }
             }
             if (workerFilter(request)) {
                 System.out.println("WORKER REQUEST");
-                if (tokenService.checkRole(token).equals("WORKER"))
+                if (tokenService.checkRole(token).equals("WORKER")) {
                     filterChain.doFilter(request, response);
+                    return;
+                }
             }
             if (microserviceFilter(request)) {
                 System.out.println("MICROSERVICE REQUEST");
-                if (tokenService.checkRole(token).equals("MICROSERVICE"))
+                if (tokenService.checkRole(token).equals("MICROSERVICE")) {
                     filterChain.doFilter(request, response);
+                    return;
+                }
             }
 
             if (!microserviceFilter(request) && !workerFilter(request) && !moderatorFilter(request) && !adminFilter(request)) {
